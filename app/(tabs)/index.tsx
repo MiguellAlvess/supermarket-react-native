5265ac4c067fdd0345c9a88270cd0caaaca15b3b
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-03-14 13:15:02 -0300

    refactor: simplify navigation to the login screen using Redirect

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,7 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import { router } from "expo-router";
+import { Redirect } from "expo-router"
 
-export default function Home() {
-
-  function irLogin() {
-    router.push("/login");
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Supermercado</Text>
-
-      <Button
-        title="Ir para Login"
-        onPress={irLogin}
-      />
-    </View>
-  );
+const TabsIndexScreen = () => {
+  return <Redirect href="/login" />
 }
 
-const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-
-  titulo: {
-    fontSize: 28,
-    fontWeight: "bold",
-    marginBottom: 20
-  }
-
-});
+export default TabsIndexScreen
